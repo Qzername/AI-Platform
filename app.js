f76a2acba3f07fff9d969b3b7ceec6846e2bede8
Hubert <40564309+Qzername@users.noreply.github.com>
2023-08-12 03:31:53 +0200

    Server modification

--- app.js
+++ app.js
@@ -6,16 +6,30 @@ const hostname = '127.0.0.1';
 const port = 8080;
 
 const server = http.createServer((req, res) => {
+  
   var q = url.parse(req.url, true);
-  var filename = "." + q.pathname;
+
+  var filename;
+
+  if(q.pathname == '/')
+    filename = "/index.html";
+  else
+    filename = "." + q.pathname;
+  
+  filename = "page/" + filename;
+
   fs.readFile(filename, function(err, data) {
+
     if (err) {
       res.writeHead(404, {'Content-Type': 'text/html'});
       return res.end("404 Not Found");
     } 
+
     res.write(data);
+
     return res.end();
   });
+
 });
 
 server.listen(port, hostname, () => {
