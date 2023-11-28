6528d8d94eeb822acc57c966c9c6c1e93bb1865b
Hubert <40564309+Qzername@users.noreply.github.com>
2023-11-29 00:21:43 +0100

    Update webRequest.js

--- AI Platform Page/page/js/webRequest.js
+++ AI Platform Page/page/js/webRequest.js
@@ -1,10 +1,11 @@
-const api = "http://localhost:5000/api/"
+const api = "https://50e6-185-93-74-95.ngrok-free.app/api/"
 
 function httpRequest(theUrl, requestType, obj = null, contentType = 'application/json')
 {
     var xmlHttp = new XMLHttpRequest();
     xmlHttp.open( requestType, theUrl, false ); // false for synchronous request
     xmlHttp.setRequestHeader('Content-type', contentType);
+    xmlHttp.setRequestHeader('ngrok-skip-browser-warning', '6024');
     xmlHttp.send( obj );
     return xmlHttp.responseText;
 }
