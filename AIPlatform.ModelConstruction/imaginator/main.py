c7384fb762ef61d3a1587d58819d8a4b22e274cf
Qzername <40564309+Qzername@users.noreply.github.com>
2025-11-17 20:23:46 +0100

    [imaginator] Add model

--- AIPlatform.ModelConstruction/imaginator/main.py
+++ AIPlatform.ModelConstruction/imaginator/main.py
@@ -1,5 +1,14 @@
 import hf_dataset
 import os
+import random
+import torch
+
+# --- CONFIG --- 
+
+manualSeed = 44
+print("Random Seed: ", manualSeed)
+random.seed(manualSeed)
+torch.manual_seed(manualSeed)
 
 # --- PREPARE DATASET ---
 
@@ -14,10 +23,12 @@ BIRDS_DATASET_IMAGE_LABEL = "jpg"
 # 1 - dog 
 # 2 - bird
 
-cats_dogs_dataset = hf_dataset.full_load("microsoft/cats_vs_dogs", CATS_DOGS_DATASET_PATH, CATS_DOGS_DATASET_IMAGE_LABEL)
-birds_dataset = hf_dataset.full_load("birder-project/CUB_200_2011", BIRDS_DATASET_PATH, BIRDS_DATASET_IMAGE_LABEL)
+dataset = []
 
 if not os.path.isdir(FINAL_DATASET_PATH) or not os.path.isdir(FINAL_DATASET_PATH+"1/"):
+    cats_dogs_dataset = hf_dataset.full_load("microsoft/cats_vs_dogs", CATS_DOGS_DATASET_PATH, CATS_DOGS_DATASET_IMAGE_LABEL)
+    birds_dataset = hf_dataset.full_load("birder-project/CUB_200_2011", BIRDS_DATASET_PATH, BIRDS_DATASET_IMAGE_LABEL)
+
     os.mkdir(FINAL_DATASET_PATH)
     for i in range(3):
         os.mkdir(f"{FINAL_DATASET_PATH}{str(i)}/")
@@ -29,3 +40,39 @@ if not os.path.isdir(FINAL_DATASET_PATH) or not os.path.isdir(FINAL_DATASET_PATH
     # save bird images
     for i, example in enumerate(birds_dataset["train"]):
         example[BIRDS_DATASET_IMAGE_LABEL].save("./dataset/2/" + str(i) + ".jpg")
+
+
+# --- MODEL & TRAINING --- 
+import model 
+from torch.utils.data import DataLoader
+
+EPOCHES = 10
+BATCH_SIZE = 32
+
+dataloader = DataLoader(dataset, BATCH_SIZE, shuffle=True)
+
+imaginator = model.Imaginator()
+
+for epoch in range(2):  # loop over the dataset multiple times
+
+    running_loss = 0.0
+    for i, data in enumerate(dataloader, 0):
+        # get the inputs; data is a list of [inputs, labels]
+        inputs, labels = data
+
+        # zero the parameter gradients
+        optimizer.zero_grad()
+
+        # forward + backward + optimize
+        outputs = net(inputs)
+        loss = criterion(outputs, labels)
+        loss.backward()
+        optimizer.step()
+
+        # print statistics
+        running_loss += loss.item()
+        if i % 2000 == 1999:    # print every 2000 mini-batches
+            print(f'[{epoch + 1}, {i + 1:5d}] loss: {running_loss / 2000:.3f}')
+            running_loss = 0.0
+
+print('Finished Training')
---
+++ AIPlatform.ModelConstruction/imaginator/model.py
@@ -0,0 +1,22 @@
+import torch.nn as nn
+
+class Imaginator(nn.Module):
+    def __init__(self):
+        super().__init__()
+        self.model = nn.Sequential(
+            nn.Conv2d(3, 64, kernel_size=5, stride=2, padding=2),
+            nn.LeakyReLU(0.2, inplace=True),
+            nn.Dropout(0.3),
+
+            nn.Conv2d(64, 128, kernel_size=5, stride=2, padding=2),
+            nn.LeakyReLU(0.2, inplace=True),
+
+            nn.Conv2d(128, 256, kernel_size=7, stride=2, padding=3),
+            nn.LeakyReLU(0.2, inplace=True),
+
+            nn.Flatten(),
+            nn.Linear(16384, 1),
+        )
+
+    def forward(self, x):
+        return self.model(x)
